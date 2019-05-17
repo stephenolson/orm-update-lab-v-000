2dac4943e394649f78b3fec97cb92d628de3f4f6
Stephen Olson <stephenolson@gmail.com>
2019-05-17 01:51:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,123 +1,63 @@
-require_relative "../config/environment.rb"
-
 class Student
-
   attr_accessor :name, :grade
   attr_reader :id
 
-    def self.new_from_db(row)
-      student = self.new
-      student.id = row[0]
-      student.name = row[1]
-      student.grade = row[2]
-      student
-    end
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
-    def self.all
-      sql = <<-SQL
-        SELECT * FROM students
-        SQL
-
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
-    end
-
-    def self.find_by_name(name)
-      sql = <<-SQL
-        SELECT *
-        FROM students
-        WHERE name = ?
-        LIMIT 1
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
       SQL
 
-      DB[:conn].execute(sql, name).map do |row|
-        self.new_from_db(row)
-      end.first
-    end
+      DB[:conn].execute(sql)
+  end
 
-    def save
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
       sql = <<-SQL
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
 
-    def self.create_table
-      sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-      )
-      SQL
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
-      DB[:conn].execute(sql)
-    end
+  def self.new_from_db(student)
+    new_student = self.new(student[0], student[1], student[2])
+    new_student
+  end
 
-    def self.drop_table
-      sql = "DROP TABLE IF EXISTS students"
-      DB[:conn].execute(sql)
-    end
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
-    def self.all_students_in_grade_9
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 9
-        SQL
-
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
-    end
-
-    def self.students_below_12th_grade
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade < 12
-        SQL
-
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
-    end
-
-    def self.first_X_students_in_grade_10(amount)
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 10
-        LIMIT ?
-        SQL
-
-      DB[:conn].execute(sql, amount).map do |row|
-        self.new_from_db(row)
-      end
-    end
-
-    def self.first_student_in_grade_10
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 10
-        LIMIT 1
-        SQL
-
-      DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end.first
-    end
-
-    def self.all_students_in_grade_X(grade)
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = ?
-      SQL
-
-      DB[:conn].execute(sql, grade).map do |row|
-        self.new_from_db(row)
-      end
-    end
-
-
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
 end
