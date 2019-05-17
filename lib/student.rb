4281d86bba0a45f6f30eb4716be35dfb794a2d4b
Stephen Olson <stephenolson@gmail.com>
2019-05-17 01:51:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade
+  attr_reader :id
 
     def self.new_from_db(row)
       student = self.new
